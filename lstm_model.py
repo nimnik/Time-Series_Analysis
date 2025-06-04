e140c754f311dba33ce50006329e52bff34893ff
nimnik <134252467+nimnik@users.noreply.github.com>
2025-06-04 10:46:31 -0700

    Updating

--- lstm_model.py
+++ lstm_model.py
@@ -1,35 +1,27 @@
-from keras.models import Sequential
-from keras.layers import LSTM, Dense
-import pandas as pd
-from sklearn.metrics import mean_squared_error
-import numpy as np
+from keras.models import Sequential # type: ignore
+from keras.layers import LSTM, Dense # type: ignore
 
-# Length of train dataset
-TRAIN_LEN = 1000
+class LSTM_Model:
+    def __init__(self, train_data, target_data, pred_size=1):
+        # Creating a variable for train data, to later use in process method
+        self.train = train_data.to_numpy().reshape(1, len(train_data), 1)
 
-# Creating a simple integer sequence to represent the dates in our train time series
-train_set = np.array([i for i in range(1, TRAIN_LEN+1)]).reshape((1,TRAIN_LEN,1))
+        # Creating a variable for target data, to later use in process method
+        self.target = target_data.to_numpy().reshape(1, len(target_data), 1)
 
-# Loading the closed price of the dataset as the main time-series data
-df = pd.read_csv("../Microsoft_Stock.csv")
+        # Number of future values to be predicted
+        self.pred_size = pred_size
+        
+    def process(self):
+        # Training LSTM on the train data
+        lstm_model = Sequential()
+        lstm_model.add(LSTM(units=8, input_shape=(len(self.train), 1)))
+        lstm_model.add(Dense(self.pred_size))
+        lstm_model.compile(loss='mean_squared_error', optimizer='adam')
 
-close_price_list = df["Close"].tolist()
-close_series = pd.Series(close_price_list)
-
-# Partitioning the data to train and test data
-train_series = close_series[:TRAIN_LEN]
-test_series = close_series[TRAIN_LEN:]
-
-# Creating a simple integer sequence to represent the dates in our test time series
-test_set = [i for i in range(TRAIN_LEN+1, TRAIN_LEN+1+len(test_series))]
-
-model = Sequential()
-model.add(LSTM(units=8, input_shape=(len(train_set),1), return_sequences=True))
-model.add(Dense(1))
-model.compile(loss='mse', optimizer='adam')
-history = model.fit(train_set, np.array(train_series).reshape((1,TRAIN_LEN)), epochs=2, batch_size=1)
-prediction = model.predict(np.array(test_set).reshape((1,len(test_series),1)))
-# print((prediction))
-
-pred_lstm = mean_squared_error(test_series, prediction.reshape((len(test_series),)))
-print(f"LSTM's prediction error is: {pred_lstm}")
+        # Predicting the future prices, using trained LSTM model
+        lstm_model.fit(self.train, self.target, epochs=2, batch_size=1, verbose=0)
+        pred_lstm = lstm_model.predict(self.train, verbose=0)
+        
+        return pred_lstm
+    
