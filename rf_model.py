ec143d18b3972fdde43ca6c9da0e9c0a1dd9519f
nimnik <134252467+nimnik@users.noreply.github.com>
2025-06-04 10:48:33 -0700

    Updating

--- rf_model.py
+++ rf_model.py
@@ -1,37 +1,25 @@
-import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
 import numpy as np
 
-# Length of train dataset
-TRAIN_LEN = 1000
+class RF_Model:
+    def __init__(self, train_data, order=(0,1,0), pred_size=1):
+        # Creating a variable for train data, to later use in process method
+        self.train = train_data
 
-# Creating a simple integer sequence to represent the dates in our train time series
-train_set = np.array([i for i in range(1, TRAIN_LEN+1)])
+        # Number of future values to be predicted
+        self.pred_size = pred_size
+        
+    def process(self, n_est=150, random_st=42):
+        # Specifying Train and Test Inputs
+        train_input = np.arange(len(self.train))
+        test_input = np.arange(len(self.train),(len(self.train)+self.pred_size))
 
-# Loading the closed price of the dataset as the main time-series data
-df = pd.read_csv("../Microsoft_Stock.csv")
+        # Training Random Forest on the train data
+        rf = RandomForestRegressor(n_estimators=n_est, random_state=random_st)
+        rf.fit(train_input.reshape(-1,1), self.train)
 
-close_price_list = df["Close"].tolist()
-close_series = pd.Series(close_price_list)
+        # Predicting the future prices, using trained Random Forest model
+        pred_rf = rf.predict(test_input.reshape(-1, 1))
+        
+        return pred_rf
 
-# Partitioning the data to train and test data
-train_series = close_series[:TRAIN_LEN]
-test_series = close_series[TRAIN_LEN:]
-
-# Defining and Running Random Forest Model
-model = RandomForestRegressor(n_estimators=100, random_state=42)
-print(train_set.shape, np.array(train_series).shape)
-
-model.fit(train_set.reshape(-1, 1), np.array(train_series).reshape(-1, 1))
-
-
-# Creating a simple integer sequence to represent the dates in our test time series
-test_set = np.array([i for i in range(TRAIN_LEN, len(close_price_list))])
-
-# Predicting the test values
-predictions = model.predict(test_set.reshape(-1, 1))
-
-pred_rf = mean_squared_error(np.array(test_series), predictions)
-print(f"Random Forest's prediction error is: {pred_rf}")
