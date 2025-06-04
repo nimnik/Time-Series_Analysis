e450b231e789a1cd8a69454baa4ce7a1f14c878b
nimnik <134252467+nimnik@users.noreply.github.com>
2025-06-04 10:47:56 -0700

    Updating

--- statistical_models.py
+++ statistical_models.py
@@ -1,39 +1,32 @@
-import pandas as pd
 from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.tsa.api import SimpleExpSmoothing
-from sklearn.metrics import mean_squared_error
 
-# Length of train dataset
-TRAIN_LEN = 1000
+class Stat_Models:
+    def __init__(self, train_data, order=(0,1,0), pred_size=1):
+        # Creating a variable for train data, to later use in process method
+        self.train = train_data
 
-# Loading the closed price of the dataset as the main time-series data
-df = pd.read_csv("../Microsoft_Stock.csv")
+        # Number of future values to be predicted
+        self.pred_size = pred_size
 
-close_price_list = df["Close"].tolist()
-close_series = pd.Series(close_price_list)
+        # Initializing ARIMA on the train data
+        self.arima = ARIMA(endog=train_data, order=order)
 
-# Partitioning the data to train and test data
-train_series = close_series[:TRAIN_LEN]
-test_series = close_series[TRAIN_LEN:]
+        # Initializing Exponential Smoothing on the train data
+        self.exp_smooth = SimpleExpSmoothing(train_data)
+        
+    def process(self, smoothing_level=0.8):
+        # Training ARIMA
+        arima_trained_model = self.arima.fit()
 
-# Training ARIMA on the train data
-arima = ARIMA(train_series, order=(1,1,1))
-arima_trained_model = arima.fit()
+        # Training Exponential Smoothing
+        exp_trained_model = self.exp_smooth.fit(smoothing_level=smoothing_level, optimized=False)
 
-# Training Exponential Smoothing on the train data
-exp_smoothing = SimpleExpSmoothing(train_series)
-exp_trained_model = exp_smoothing.fit(smoothing_level=0.4, optimized=True)
+        # Predicting the future prices, using trained ARIMA model
+        pred_arima = arima_trained_model.predict(start=len(self.train), end=len(self.train)+self.pred_size-1)
 
-# Predicting the future prices, using trained ARIMA model
-pred_arima = arima_trained_model.predict(start=TRAIN_LEN, end=len(close_series)-1)
+        # Predicting the future prices, using trained Exponential Smoothing model
+        pred_exp = exp_trained_model.forecast(self.pred_size)
+        
+        return pred_arima, pred_exp
 
-# Predicting the future prices, using trained Exponential Smoothing model
-pred_exp = exp_trained_model.forecast(len(test_series))  #.predict(start=TRAIN_LEN, end=len(close_series)-1)
-
-# Calculating the Error of ARIMA's prediction
-pred_err_arima = mean_squared_error(test_series, pred_arima)
-print(f"ARIMA's prediction error is: {pred_err_arima}")
-
-# Calculating the Error of Exponential Smoothing's prediction
-pred_err_exp = mean_squared_error(test_series, pred_exp)
-print(f"Exponential Smoothing's prediction error is: {pred_err_exp}")
